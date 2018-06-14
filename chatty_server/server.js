69c4c5311a860374040c8e1f963ee7c899a6e09b
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-06-14 11:49:29 -0400

    updated active users to change on disconnect

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,64 +1,66 @@
-
-const express = require('express');
-const SocketServer = require('ws').Server;
-const uuidv4 = require('uuid/v4')
+const express = require("express");
+const SocketServer = require("ws").Server;
+const uuidv4 = require("uuid/v4");
 
 // Set the port to 3001
 const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static("public"))
+  .listen(PORT, "0.0.0.0", "localhost", () =>
+    console.log(`Listening on ${PORT}`)
+  );
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
-
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
-  console.log('Client connected');
+wss.on("connection", ws => {
+  console.log("Client connected");
   users = {
     type: "userCountStatus",
     userCount: wss.clients.size
   };
-  console.log(users)
+  console.log(users);
   wss.clients.forEach(client => {
     client.send(JSON.stringify(users));
   });
   // console.log('WYYYEYYEY', ws)
   // console.log('CHECK ONE', numberOfClients)
 
-  ws.on('message', message => {
-    console.log('1234324',message)
+  ws.on("message", message => {
+    console.log("1234324", message);
     const uuid = uuidv4();
-    console.log(uuid)
-    const newMessage = JSON.parse(message)
-    newMessage.id = uuid
-    if (newMessage.type === 'postMessage') {
-      newMessage.type = 'incomingMessage'
+    console.log(uuid);
+    const newMessage = JSON.parse(message);
+    newMessage.id = uuid;
+    if (newMessage.type === "postMessage") {
+      newMessage.type = "incomingMessage";
     } else {
-      newMessage.type = 'incomingNotification'
+      newMessage.type = "incomingNotification";
     }
-    console.log('SERVER SIDE', newMessage)
+    console.log("SERVER SIDE", newMessage);
     // console.log(`User ${newMessage.id} ${newMessage.username} said ${newMessage.content}`)
 
-    
     wss.clients.forEach(client => {
       client.send(JSON.stringify(newMessage, wss.clients.size));
-    })
-  })
+    });
+  });
 
-  
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => {
-    console.log('Clients are now: ', wss.clients.size)
-    const disconnectedClient = wss.clients.size
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    users = {
+      type: "userCountStatus",
+      userCount: wss.clients.size
+    };
+    wss.clients.forEach(client => {
+      client.send(JSON.stringify(users));
+    });
     // this.send(JSON.stringify(disconnectedClient))
-console.log('Client disconnected');
-
-  })
+  });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Nav from './navBar.jsx'
-import ChatBar from './ChatBar.jsx'
-import MessageList from './MessageList.jsx'
+import Nav from "./navBar.jsx";
+import ChatBar from "./ChatBar.jsx";
+import MessageList from "./MessageList.jsx";
 import { IncomingMessage } from "http";
 
 class App extends Component {
@@ -10,39 +10,46 @@ class App extends Component {
 
     this.state = {
       loading: true,
-        currentUser: {name: "Anonymous"}, // optional. if currentUser is not defined, it means the user is Anonymous
-        messages: [],
-        activeUsers: {}
-      }
+      currentUser: { name: "Anonymous" }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [],
+      activeUsers: {}
+    };
   }
 
   componentDidMount() {
-    this.socket = new WebSocket('ws://localhost:3001');
+    this.socket = new WebSocket("ws://localhost:3001");
 
-    this.socket.onmessage = (event) => {      // console.log('BLOOOP BLAAAP', event);
-      const parsedEvent = JSON.parse(event.data)
+    this.socket.onmessage = event => {
+      // console.log('BLOOOP BLAAAP', event);
+      const parsedEvent = JSON.parse(event.data);
 
-      if (parsedEvent.type === 'incomingNotification'){
-        parsedEvent.content = `${parsedEvent.oldname} changed their name to ${parsedEvent.username}`
-      } else if (parsedEvent.type === 'userCountStatus') {
-        this.setState({activeUsers:parsedEvent})
+      if (parsedEvent.type === "incomingNotification") {
+        parsedEvent.content = `${parsedEvent.oldname} changed their name to ${
+          parsedEvent.username
+        }`;
+      } else if (parsedEvent.type === "userCountStatus") {
+        this.setState({ activeUsers: parsedEvent });
       }
-      const activeUsers = parsedEvent
-      console.log('ASDKJHSKJDHA', activeUsers)
-      const newMessage = {id: parsedEvent.id, username: parsedEvent.username, content: parsedEvent.content, type: parsedEvent.type};
+      const activeUsers = parsedEvent;
+      console.log("ASDKJHSKJDHA", activeUsers);
+      const newMessage = {
+        id: parsedEvent.id,
+        username: parsedEvent.username,
+        content: parsedEvent.content,
+        type: parsedEvent.type
+      };
 
-      const messages = this.state.messages.concat(newMessage)
-      console.log('WAAAATSSSSS THIS', messages)
-      
+      const messages = this.state.messages.concat(newMessage);
+      console.log("WAAAATSSSSS THIS", messages);
 
       this.setState({
-        messages:messages
-      })
+        messages: messages
+      });
       // code to handle incoming message
-    }
-    this.socket.onopen = (event) => {
+    };
+    this.socket.onopen = event => {
       // console.log(event)
-      console.log("Connected to Server")
+      console.log("Connected to Server");
     };
     // console.log('Connected to server')
     console.log("componentDidMount <App />");
@@ -56,30 +63,42 @@ class App extends Component {
     //   this.setState({messages: messages})
     // }, 3000);
   }
-  
+
   _submitMessage = (username, inputMessage) => {
-    const newMessage = {username: username, content: inputMessage, type: 'postMessage'};
+    const newMessage = {
+      username: username,
+      content: inputMessage,
+      type: "postMessage"
+    };
     // console.log('CLIENT SIDE', newMessage)
     // const messages = this.state.messages.concat(newMessage)
     // this.setState({messages:messages})
-    this.socket.send(JSON.stringify(newMessage))
-  }
+    this.socket.send(JSON.stringify(newMessage));
+  };
 
-  _submitUser = (username) => {
-    const newUser = {name: username};
-    const notificationObject = {oldname: this.state.currentUser.name, username: username, type: 'postNotification'}
+  _submitUser = username => {
+    const newUser = { name: username };
+    const notificationObject = {
+      oldname: this.state.currentUser.name,
+      username: username,
+      type: "postNotification"
+    };
     // console.log('CHECKING THIS MATE', typeof newUser)
-    this.socket.send(JSON.stringify(notificationObject))
+    this.socket.send(JSON.stringify(notificationObject));
     // console.log(notificationObject)
-    this.setState({currentUser: newUser})
-  }
+    this.setState({ currentUser: newUser });
+  };
 
-  render() { 
+  render() {
     return (
       <div>
         <Nav activeUsers={this.state.activeUsers} />
-        <MessageList messages={this.state.messages}/>
-        <ChatBar submitUser = {this._submitUser} submitMessage={this._submitMessage} currentUser={this.state.currentUser}/>
+        <MessageList messages={this.state.messages} />
+        <ChatBar
+          submitUser={this._submitUser}
+          submitMessage={this._submitMessage}
+          currentUser={this.state.currentUser}
+        />
       </div>
     );
   }
